859b19a97402a9a7bc97eb5a860415dabbabd584
apoorv1316 <tiwari.apoorv1316@gmail.com>
2020-08-01 20:13:10 +0530

    class Level and State created

---
+++ game.js
@@ -0,0 +1,46 @@
+// Plan for a small level
+let simpleLevelPlan = `
+......................
+..#................#..
+..#..............=.#..
+..#.........o.o....#..
+..#.@......#####...#..
+..#####............#..
+......#++++++++++++#..
+......##############..
+......................`;
+
+class Level {
+  constructor(plan) {
+    // trim the whitespace at start and at end of string and split the remaining string on newline characters and each line of plan is spread into an array, making array of characters
+    let rows = plan
+      .trim()
+      .split("\n")
+      .map((l) => [...l]);
+    this.height = rows.length;
+    this.width = rows[0].length;
+    this.startActors = [];
+
+    this.rows = rows.map((row, y) => {
+      return row.map((ch, x) => {
+        let type = levelChars[ch];
+        if (typeof type == "string") return type;
+        this.startActors.push(type.create(new Vec(x, y), ch));
+        return "empty";
+      });
+    });
+  }
+}
+class State {
+  constructor(level, actors, status) {
+    this.level = level;
+    this.actors = actors;
+    this.status = status;
+  }
+  static start(level) {
+    return new State(level, level.startActors, "playing");
+  }
+  get player() {
+    return this.actors.find((a) => a.type == "player");
+  }
+}
